5b2f8c24085966a9e5ce5fd4278e398b4e430f5f
bribbon <s.jordi@emubox.ch>
2021-03-23 20:43:28 +0100

    Add files via upload

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,10 @@ cd /home/container
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
-# display nodejs version on dockerstart
-echo "\r\n"
-echo -e "Node: \033[46m${node -v}\033[0m"
-echo "\r\n"
+# Display noDejs version on dockerstart
+echo " "
+echo -e "Node: \033[46mnode -v\033[0m"
+echo " "
 
 # Run the Server
 eval ${MODIFIED_STARTUP}
