f14c99b8747ef6a5dc64d6d2028f01d51bf96b14
bribbon <s.jordi@emubox.ch>
2021-03-23 20:54:12 +0100

    Add files via upload

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 cd /home/container
 
-# Make internal Docker IP address available to processes.
-export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
-
 # Replace Startup Variables
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Display noDejs version on dockerstart
+echo " "
+node -v
+echo " "
 
 # Run the Server
 eval ${MODIFIED_STARTUP}
