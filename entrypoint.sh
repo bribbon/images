24180f889320f4bef634390e1acb2b0d66a38008
bribbon <s.jordi@emubox.ch>
2021-03-23 19:43:00 +0100

    Add files via upload

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 cd /home/container
-# Make internal Docker IP address available to processes.
-node -v
 # Replace Startup Variables
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo ":/home/container$ ${MODIFIED_STARTUP}"
